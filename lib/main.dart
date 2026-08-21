5b05ef956ce4b315ba77b3e7ff37df48f8d3b1f7
hanadi92 <hanadi.tamimi@gmail.com>
2026-08-21 22:08:05 +0300

    feat: dark theme
    Signed-off-by: hanadi92 <hanadi.tamimi@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,51 @@ void main() {
   runApp(MyApp(apiService: ApiService(http.Client())));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({required this.apiService, super.key});
 
   final ApiService apiService;
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  void _toggleTheme() {
+    setState(() {
+      _themeMode = _themeMode == ThemeMode.dark
+          ? ThemeMode.light
+          : ThemeMode.dark;
+    });
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chiano',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        colorScheme: .fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.light,
+        ),
       ),
-      home: HomeScreen(apiService: apiService,),
+      darkTheme: ThemeData(
+          useMaterial3: true,
+          colorScheme: .fromSeed(
+            seedColor: Colors.deepPurple,
+            brightness: Brightness.dark,
+          )
+      ),
+      themeMode: _themeMode,
+      home: HomeScreen(
+        apiService: widget.apiService,
+        onToggleTheme: _toggleTheme,
+        isDarkMode: _themeMode == ThemeMode.dark,
+      ),
     );
   }
 }
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -4,13 +4,36 @@ import '../services/api_service.dart';
 import 'form_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({required this.apiService, super.key});
+  const HomeScreen({
+    required this.apiService,
+    required this.onToggleTheme,
+    required this.isDarkMode,
+    super.key,
+  });
 
   final ApiService apiService;
+  final VoidCallback onToggleTheme;
+  final bool isDarkMode;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chiano'),
+        actions: [
+          IconButton(
+            onPressed: onToggleTheme,
+            icon: Icon(
+              isDarkMode
+                  ? Icons.light_mode
+                  : Icons.dark_mode,
+            ),
+            tooltip: isDarkMode
+                ? 'Switch to light mode'
+                : 'Switch to dark mode',
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Center(
           child: Padding(

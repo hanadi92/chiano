9d3faa1f03bbec0c6a55dc9a7b735e68e2613f63
hanadi92 <hanadi.tamimi@gmail.com>
2026-08-22 13:33:05 +0300

    build: fix android building
    Signed-off-by: hanadi92 <hanadi.tamimi@gmail.com>

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
+    }
+}
+
 allprojects {
     repositories {
         google()
